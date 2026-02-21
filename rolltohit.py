1f728980507539f806f70057b0e0b23a488707b5
Dylan Morter <morted5815@clarkstate.edu>
2026-02-21 06:08:00 -0500

    Update rolltohit.py

--- rolltohit.py
+++ rolltohit.py
@@ -3,3 +3,21 @@
 #Roll the correct number of dice, then display the results
 #This is a guided practice. Either follow with the video or your instructor will
 #go over this in class.
+
+import random
+
+random.seed()
+
+diceCount = int(input("How many dice to be rolled: "))
+hitTarget = int(input("What is the hit target: "))
+
+hitCount = 0
+diceRoll = 0
+
+for i in range(diceCount):
+    diceRoll = random.randint(1,6)
+    print(f"Roll is {diceRoll}.")
+    if diceRoll >= hitTarget:
+        hitCount += 1
+
+print(f"{diceCount} dice were rolled with a hit target of {hitTarget}, and it hit {hitCount} times.")
